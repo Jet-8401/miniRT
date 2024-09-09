f44f5d387861a0b4420caa9188713b6dbf33759c
Julien Lopez <jullopez@c3r3s5.42luxembourg.lu>
2024-09-09 16:15:41 +0200

    backup

--- sources/render.c
+++ sources/render.c
@@ -12,67 +12,67 @@
 
 #include "../headers/minirt.h"
 
-void init_camera(t_scene *scene)
+void	init_camera(t_scene *scene)
 {
-    t_screen *screen;
+	t_screen	*screen;
 
-    screen = &scene->screen;
-    screen->screen_width = WIDTH;
-    screen->screen_height = HEIGHT;
-    screen->scale = tan((float) scene->cam->fov / 2 * M_PI / 180);
-    screen->aspect_ratio = screen->scale / ((float) WIDTH / HEIGHT);
-    screen->right = normalize(merge_vect(scene->cam->dir, (t_vec3){0.0, 1.0, 0.0}));
-    screen->up = normalize(merge_vect(scene->cam->dir, screen->right));
-    screen->right = normalize(merge_vect(scene->cam->dir, screen->up));
+	screen = &scene->screen;
+	screen->screen_width = WIDTH;
+	screen->screen_height = HEIGHT;
+	screen->scale = tan((float) scene->cam->fov / 2 * M_PI / 180);
+	screen->aspect_ratio = screen->scale / ((float) WIDTH / HEIGHT);
+	screen->right = normalize(merge_vect(scene->cam->dir,
+				(t_vec3){0.0, 1.0, 0.0}));
+	screen->up = normalize(merge_vect(scene->cam->dir, screen->right));
+	screen->right = normalize(merge_vect(scene->cam->dir, screen->up));
 }
 
-int render_scene(t_scene *scene)
+int	render_scene(t_scene *scene)
 {
-    pixel_draw(scene, &scene->render);
-    mlx_put_image_to_window(scene->mlx.mlx, scene->mlx.win, scene->mlx.img.img, 0, 0);
-    fps_display(&scene->mlx);
-    render_time_display(&scene->mlx);
-    return (0);
+	pixel_draw(scene, &scene->render);
+	mlx_put_image_to_window(scene->mlx.mlx, scene->mlx.win,
+		scene->mlx.img.img, 0, 0);
+	fps_display(&scene->mlx);
+	render_time_display(&scene->mlx);
+	return (0);
 }
 
-void pixel_draw(t_scene *scene, t_render *render)
+void	pixel_draw(t_scene *scene, t_render *render)
 {
-    uint64_t	x;
-    uint64_t	y;
+	uint64_t	x;
+	uint64_t	y;
 
-    y = 0;
-    while (y < HEIGHT)
-    {
-        x = 0;
-        while (x < WIDTH)
-        {
-            new_init_camera(scene, &render->prime_ray, (float)x, (float)y);
-            render->color_ambiant = color_rgb(ambiant_color(render, scene));
-            new_mlx_pixel_put(&scene->mlx, x, y, render->color_ambiant);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			new_init_camera(scene, &render->prime_ray, (float)x, (float)y);
+			render->color_ambiant = color_rgb(ambiant_color(render, scene));
+			new_mlx_pixel_put(&scene->mlx, x, y, render->color_ambiant);
+			x++;
+		}
+		y++;
+	}
 }
 
-void new_init_camera(t_scene *scene, t_ray_view *prime_ray, float x, float y)
+void	new_init_camera(t_scene *scene, t_ray_view *prime_ray, float x, float y)
 {
-    t_scene *tmp_scene;
-    t_vec3 coord;
-    t_vec3 vertical;
-    t_vec3 horizontal;
-    t_vec3 res;
+	t_vec3	vertical;
+	t_vec3	horizontal;
+	t_vec3	res;
 
-    tmp_scene = scene;
-    prime_ray->origin = new_vector(tmp_scene->cam->pos.x, tmp_scene->cam->pos.y, tmp_scene->cam->pos.z);
-    coord.x = ((2.0f * x) / WIDTH) - 1;
-    coord.y = ((2.0f * y) / HEIGHT) - 1;
-    coord.z = 0;
-    vertical = mult_vec3(tmp_scene->screen.up, coord.y * tmp_scene->screen.aspect_ratio);
-    horizontal = mult_vec3(tmp_scene->screen.right, coord.x * tmp_scene->screen.scale);
-    res = add_vec3(vertical, horizontal);
-    res = add_vec3(res, scene->cam->dir);
-    res = add_vec3(res, scene->cam->pos);
-    prime_ray->direction = sub_vec3(res, prime_ray->origin);
-    normalize_bis(&prime_ray->direction);
+	prime_ray->origin = scene->cam->pos;
+	prime_ray->direction.x = ((2.0f * x) / WIDTH) - 1;
+	prime_ray->direction.y = ((2.0f * y) / HEIGHT) - 1;
+	vertical = mult_vec3(scene->screen.up, prime_ray->direction.y
+			* scene->screen.aspect_ratio);
+	horizontal = mult_vec3(scene->screen.right, prime_ray->direction.x
+			* scene->screen.scale);
+	res = add_vec3(vertical, horizontal);
+	res = add_vec3(res, scene->cam->dir);
+	res = add_vec3(res, scene->cam->pos);
+	prime_ray->direction = sub_vec3(res, prime_ray->origin);
+	vec3_normalize(&prime_ray->direction);
 }
--- sources/vec3D_utils.c
+++ sources/vec3D_utils.c
@@ -48,8 +48,8 @@ void	vec3_normalize(t_vec3 *vec)
 {
 	double	magnitude;
 
-	magnitude = sqrt(((vec->x * vec->x) + (vec->y * vec->y) +
-		(vec->z * vec->z)));
+	magnitude = sqrt(((vec->x * vec->x) + (vec->y * vec->y)
+		+ (vec->z * vec->z)));
 	vec->x /= magnitude;
 	vec->y /= magnitude;
 	vec->z /= magnitude;
